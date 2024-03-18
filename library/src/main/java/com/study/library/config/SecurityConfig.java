934006b82dcb78107e4647796c9b7a03dc859de2
ez0603 <dlwldjs3132@naver.com>
2024-03-18 17:45:33 +0900

    240318

--- library/src/main/java/com/study/library/config/SecurityConfig.java
+++ library/src/main/java/com/study/library/config/SecurityConfig.java
@@ -1,15 +1,27 @@
 package com.study.library.config;
 
+import com.study.library.security.exception.AuthEntryPoint;
+import com.study.library.security.filter.JwtAuthenticationFilter;
+import com.study.library.security.filter.PermitAllFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @EnableWebSecurity // security를 따라가는게 아닌 만든걸로
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PermitAllFilter permitAllFilter;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -17,11 +29,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors(); // WebMvcConfig에 있는 크로스오리진을 설정하기 위해선 꼭 필요
         http.csrf().disable(); // csrf = SSR일때만 가능, 만든 페이지는 클라이언트사이드렌더링이기 때문에 disable을 해줌
         http.authorizeRequests()
                 .antMatchers("/server/**", "/auth/**")
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .addFilterAfter(permitAllFilter, LogoutFilter.class) // 순서는 필터 먼저 동작 / permitAllFilter = 토큰이 필요한지 거르기위한단계
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint);
     }
 }
